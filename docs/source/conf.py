5d31d6c3278a00bcca9bb1073bdc33a1af4b8aa5
EChung0111 <108372461+EChung0111@users.noreply.github.com>
2025-04-03 12:08:28 -0400

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -11,6 +11,7 @@ import sys
 
 sys.path.insert(0, os.path.abspath('../../'))
 sys.path.insert(0, os.path.abspath('../../matmacore'))
+sys.path.insert(0, os.path.abspath('../../../'))
 
 import requests
 
