a90f1f789bbf5ddb2162e73d551583bca2eab5c8
EChung <eugene.chung76@login.cuny.edu>
2025-04-07 11:49:28 -0400

    Added reverse option for choosing color maps

--- setup.py
+++ setup.py
@@ -4,15 +4,6 @@ import dynamic_versioning
 version_num = '0.1.10'
 
 setup(
-    name='matmacore',
-    version=version_num,
-    packages=find_packages(),
-    install_requires=[
-        'numpy',
-        'matplotlib',
-        'colormaps ',
-        'networkx'
-    ],
     cmdclass={
         "egg_info": dynamic_versioning.DynamicVersioningEggInfo
     }
