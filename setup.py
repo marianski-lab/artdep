71cc2a080fc819bfc9268093665cafafc8d656b9
EChung0111 <108372461+EChung0111@users.noreply.github.com>
2025-04-07 12:32:33 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import dynamic_versioning
 setup(
     name='matmacore',
     packages=find_packages(),
-    version="0.1.12",
+    version="0.1.11",
     install_requires=[
         'numpy',
         'matplotlib',
