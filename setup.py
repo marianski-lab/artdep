c4987fc9cb7b4c237530019ecff25a95a3ac34e0
EChung <eugene.chung76@login.cuny.edu>
2025-04-07 12:08:32 -0400

    updated pyproject.toml

--- setup.py
+++ setup.py
@@ -4,6 +4,12 @@ import dynamic_versioning
 version_num = '0.1.10'
 
 setup(
+    install_requires=[
+        'numpy',
+        'matplotlib',
+        'colormaps ',
+        'networkx'
+    ],
     cmdclass={
         "egg_info": dynamic_versioning.DynamicVersioningEggInfo
     }
