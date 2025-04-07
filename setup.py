3cbb7e8f3f0e3a9e7de5ca16aebb6c93ef3283fb
EChung <eugene.chung76@login.cuny.edu>
2025-04-07 16:25:02 -0400

    Bump version: 0.1.18 → 0.1.19

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='matmacore',
     packages=find_packages(),
-    version='0.1.18',
+    version='0.1.19',
     install_requires=[
         'numpy',
         'matplotlib',
