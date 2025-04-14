6af74c8c062940d5fb0daa49feef08652ea8fcc8
EChung0111 <108372461+EChung0111@users.noreply.github.com>
2025-04-14 16:08:00 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,6 @@ from setuptools import setup, find_packages
 setup(
     name='matmacore',
     packages=find_packages(),
-    version='0.1.19',
     install_requires=[
         'numpy',
         'matplotlib',
